7772d0cdfbde828fff20a2c4d34b8162ad043b5b
itachi1706 <kennethsohyq@gmail.com>
2017-02-11 21:42:25 +0800

    Created a basic tiling framework

--- app/src/main/java/com/itachi1706/cheesecakeutilities_tiles/ConfigurationActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities_tiles/ConfigurationActivity.java
@@ -1,11 +1,16 @@
 package com.itachi1706.cheesecakeutilities_tiles;
 
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.crashlytics.android.Crashlytics;
@@ -46,14 +51,33 @@ public class ConfigurationActivity extends AppCompatActivity {
      * This fragment shows general preferences only. It is used when the
      * activity is showing a two-pane settings UI.
      */
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
+    public static class GeneralPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
             setHasOptionsMenu(true);
 
+            String[] toggleList = getResources().getStringArray(R.array.toggleList);
 
+            for (String pref : toggleList) {
+                Preference p = findPreference(pref);
+                if (!(p instanceof SwitchPreference)) continue;
+
+                p.setOnPreferenceChangeListener(this);
+                String c = p.getExtras().getString("class", "-");
+                Class s;
+                try {
+                    s = Class.forName(c);
+                } catch (ClassNotFoundException e) {
+                    Log.e("ClassParsing", "Invalid Class: " + c);
+                    continue;
+                }
+
+                ((SwitchPreference) p).setChecked((getActivity().getPackageManager()
+                        .getComponentEnabledSetting(new ComponentName(getActivity(), s)) == 1));
+
+            }
         }
 
         @Override
@@ -64,6 +88,39 @@ public class ConfigurationActivity extends AppCompatActivity {
                 return true;
             }
             return super.onOptionsItemSelected(item);
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (!(preference instanceof SwitchPreference)) {
+                return false;
+            }
+
+            String c = preference.getExtras().getString("class", "-");
+            if (c.startsWith(".")) c = "com.itachi1706.cheesecakeutilities_tiles" + c;
+            Log.d("DEBUG", c);
+            Class s;
+            try {
+                s = Class.forName(c);
+            } catch (ClassNotFoundException e) {
+                Log.e("ClassParsing", "Invalid Class: " + c);
+                return false;
+            }
+
+            Boolean val = (Boolean) newValue;
+
+            toggleTile(val, s);
+
+            Log.i("TileChange", "Tile: " + preference.getTitle() + " " + ((val) ? "enabled" : "disabled"));
+            return true;
+        }
+
+        private void toggleTile(boolean val, Class c) {
+            PackageManager manager = getActivity().getPackageManager();
+            manager.setComponentEnabledSetting(new ComponentName(getActivity(), c), (val) ?
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
         }
     }
 }
